23f1fa5e6419a21d1293e3eabda47873820802aa
kohei abe <k_913411@yahoo.co.jp>
2021-02-09 22:11:17 +0900

    finish menu_button

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,29 +1,29 @@
-import { useState, useEffect } from 'react';
+import { useState} from 'react';
 import styles from './index.module.scss';
 export const Header = () => {
   const [isOpenDrawer, setDrawerState] = useState(false)
-  const [isHamburger, setHamburgerColor] = useState(false)
   const drawerFunction = () => {
     setDrawerState(!isOpenDrawer)
   }
   return (
     <header className={styles.header}>
-      <h1>Beauty Salon</h1>
-      <div className={`${styles.hamburger} ${isHamburger && styles.gray}`} onClick={() => drawerFunction()}>
+      <div className={styles.hamburger} onClick={() => drawerFunction()}>
         <span className={`${styles.bar1} ${isOpenDrawer && styles.open}`}></span>
         <span className={`${styles.bar2} ${isOpenDrawer && styles.open}`}></span>
-        <span className={`${styles.bar3} ${isOpenDrawer && styles.open}`}></span>
+        <p className={`${styles.bar3} ${isOpenDrawer && styles.open}`}>Menu</p>
       </div>
       <div className={`${styles.drawerMenu} ${isOpenDrawer && styles.open}`} onClick={() => setDrawerState(!isOpenDrawer)}>
-        <h2>Site Menu</h2>
+        <div>
+          <span></span>
+          <span></span>
+        </div>
         <ul>
-          <li><span>INTRODUCTION</span></li>
-          <li><span>MENUS</span></li>
-          <li><span>STAFF</span></li>
-          <li><span>ACCESS</span></li>
-          <li><span>CONTACT</span></li>
+          <li><span>メニュー１</span></li>
+          <li><span>メニュー２</span></li>
+          <li><span>メニュー３</span></li>
+          <li><span>メニュー４</span></li>
+          <li><span>メニュー５</span></li>
         </ul>
-        <p onClick={() => drawerFunction()}><u>閉じる</u></p>
       </div>
     </header>
   )
