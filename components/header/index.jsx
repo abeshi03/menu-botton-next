06e16eabea6261e412ce659a36845c9d0bf6ce19
kohei abe <k_913411@yahoo.co.jp>
2021-02-10 21:03:50 +0900

    finish humburger

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,30 +1,118 @@
-import { useState} from 'react';
-import styles from './index.module.scss';
-export const Header = () => {
-  const [isOpenDrawer, setDrawerState] = useState(false)
-  const drawerFunction = () => {
-    setDrawerState(!isOpenDrawer)
+import styles from "./index.module.scss";
+import Link from "next/link";
+import React, {useState} from "react"
+export default function Header() {
+  const [openMenu, setOpenMenu] = useState(false);
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
   }
   return (
-    <header className={styles.header}>
-      <div className={styles.hamburger} onClick={() => drawerFunction()}>
-        <span className={`${styles.bar1} ${isOpenDrawer && styles.open}`}></span>
-        <span className={`${styles.bar2} ${isOpenDrawer && styles.open}`}></span>
-        <p className={`${styles.bar3} ${isOpenDrawer && styles.open}`}>Menu</p>
-      </div>
-      <div className={`${styles.drawerMenu} ${isOpenDrawer && styles.open}`} onClick={() => setDrawerState(!isOpenDrawer)}>
-        <div>
-          <span></span>
-          <span></span>
+    <React.Fragment>
+      <header id="header" className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a className={styles.logo}>ハンバーガー♡</a>
+          </Link>
         </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+              <a>メニュー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+              <a>メニュー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+              <a>メニュー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+              <a>メニュー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+              <a>メニュー</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <a>メニュー</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.container}>
+          <div className={styles.humburger} onClick={() => menuFunction()}>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <p className={openMenu ? styles.open : undefined}>Menu</p>
+          </div>
+        </div>
+      </header>
+      <div className={`${styles.drawerMenu} ${openMenu ? styles.open : undefined}`}>
         <ul>
-          <li><span>メニュー１</span></li>
-          <li><span>メニュー２</span></li>
-          <li><span>メニュー３</span></li>
-          <li><span>メニュー４</span></li>
-          <li><span>メニュー５</span></li>
+          <div className={styles.close} onClick={() => menuFunction()}>
+            <span></span>
+            <span></span>
+            <p>Close</p>
+          </div>
+          <li>
+            <Link href="/">
+              <a>
+                <p className={styles.mainTitle}>メニュー</p>
+                <p className={styles.subTitle}>私のメニュー</p>
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>
+              <p className={styles.mainTitle}>メニュー</p>
+              <p className={styles.subTitle}>私のメニュー</p>
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>
+              <p className={styles.mainTitle}>メニュー</p>
+              <p className={styles.subTitle}>私のメニュー</p>
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>
+              <p className={styles.mainTitle}>メニュー</p>
+              <p className={styles.subTitle}>私のメニュー</p>
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+            <a>
+              <p className={styles.mainTitle}>メニュー</p>
+              <p className={styles.subTitle}>私のメニュー</p>
+            </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+            <a>
+              <p className={styles.mainTitle}>メニュー</p>
+              <p className={styles.subTitle}>私のメニュー</p>
+            </a>
+            </Link>
+          </li>
         </ul>
       </div>
-    </header>
+    </React.Fragment>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Header } from "../components/header"
+import Header from "../components/header"
 
 export default function Home() {
   return (
